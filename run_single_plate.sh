8fbc046e63755c391d982194410a54118b8418e2
Mehmet Tekman <mtekman89@gmail.com>
2018-03-31 20:03:06 +0200

    completed other methods

--- run_single_plate.sh
+++ run_single_plate.sh
@@ -2,36 +2,43 @@
 
 # This script produces a count matrix that is valid for only one plate
 # To run this for multiple plates, please call this multiple times with
-# different plate_names 
+# different plate_names
 plate_name=$1
 input_dir=$2
-working_dir=$3
+working_dir=wd/$plate_name
 
 R1_fastq=$(find $input_dir -type f -name "*${plate_name}*.fastq*" | grep "R1" )
 R2_fastq=$(find $input_dir -type f -name "*${plate_name}*.fastq*" | grep "R2" )
-    
+
 if [ "$R1_fastq" = "" ] || [ "$R2_fastq" = "" ]; then
     echo "Cannot find R1 or R2 for $plate_name"
     exit -1
 fi
-
-[ "$working_dir" = "" ] && echo "Working directory not set." && exit -1
-
 mkdir -p $working_dir
 
 echo "Processing: $plate_name with R1 = [$R1_fastq] and R2 = [$R2_fastq]"
 sleep 3
 
+# Load bin config and file basenames
+source single_plate.config
+
+# Quick check to see whether directory has already been processed
+if [ -e $working_dir/$counts_matrix ]; then
+    echo "Already processed $plate_name, skipping!"
+    exit 0
+fi
+
+# Load env
 source $activate propsandwich
 
-# @Takes FASTQ from a single plate and generates a list of barcodes
+# Takes FASTQ from a single plate and generates a list of barcodes
 function umitools_whitelist_plate {
     local R1_fastq=$1  # We only actually need R1 for the barcodes
     local R2_fastq=$2
 
     local white_1=$working_dir/$whitelist_out
     local white_2=$working_dir/$whitelist_raw
-    
+
     umi_tools whitelist\
               --bc-pattern=$bc_pattern\
               --stdin=$R1_fastq\
@@ -65,12 +72,12 @@ function umitools_extract_plate {
               --whitelist=$white
 
     # R1_fix and R2_fix generated, but R2_fix is useful to us
-    
+
 }
 
 function rnastar_map {
-    input_sequences=$1
-    
+    local input_sequences=$1
+
     STAR --runThreadN $star_threads\
          --genomeDir $star_index\
          --readFilesIn $input_sequences\
@@ -79,14 +86,43 @@ function rnastar_map {
          --outSAMtype BAM SortedByCoordinate
 }
 
+function featurecounts {
+    local star_bam=$1
+    featureCounts \
+        -a $geneset \
+        -o gene_assigned \
+        -R BAM $star_bam \
+        -T $fc_threads
+}
+
+function samtool_sort_index {
+    local bam_fc=$1
+    local sorted_indexed_bam=$working_dir/$bam_samtool_sortindex
+    samtools sort $bam_fc -o $sorted_indexed_bam
+    samtools index $sorted_indexed_bam
+
+    echo $sorted_indexed_bam
+}
+
+function umitools_count {
+    local sorted_bam=$1
+    local cm=$working_dir/$counts_matrix
+    umi_tools count\
+              --per-gene\
+              --gene-tag=XT\
+              --per-cell\
+              -I $sorted_bam\
+              -S $cm
+    echo $cm
+}
 
 function processPlate {
     # All functions specify the desired output file as the first argument
-    # - Also, all files are tempfiles generated in a dir with plenty of space    
+    # - Also, all files are tempfiles generated in a dir with plenty of space
     echo "\n\nPlate $plate_name" &&
         echo "Detecting barcodes..." &&
         # make whitelist_raw
-        umitools_whitelist_plate $R1_fastq $R2_fastq &&  
+        umitools_whitelist_plate $R1_fastq $R2_fastq &&
         echo -e "\nExtracting barcodes..." &&
         # make R1 and R2 fixedfastq
         umitools_extract_plate $R1_fastq $R2_fastq $whitelist_raw &&
@@ -102,11 +138,9 @@ function processPlate {
         echo -e "\nCounting molecules..." &&
         # make counts_matrix
         umitools_count $bam_sorted
-
-    echo $counts_matrix
 }
 
-source $deactivate source propsandwich
+source $deactivate propsandwich
 
 
 
@@ -124,7 +158,7 @@ source $deactivate source propsandwich
 #     R2_fastq=$2
 #     barcodes=$3
 #     outp_dir=$4
-    
+
 #     je demultiplex\
 #        F1=$R1_fastq F2=$R2_fastq\
 #        SAME_HEADERS=false\
